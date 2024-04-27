882cde31cbc33ae6a7578224917ac1ae3c2b189f
Hanatomizu <chart11from21@outlook.com>
2024-04-27 11:55:49 +0800

    ci: remove Github Action and build on local by publish.sh

--- content/readme.md
+++ content/readme.md
@@ -4,4 +4,5 @@ date = 2024-04-27T09:56:04+08:00
 draft = false
 +++
 
-# Hello there!
+## Hello there!
+
---
+++ publish.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+if [ "`git status -s`" ]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
+
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
+
+echo "Checking out gh-pages branch into public"
+git worktree add -B gh-pages public origin/gh-pages
+
+echo "Removing existing files"
+rm -rf public/*
+
+echo "Generating site"
+env HUGO_ENV="production" hugo -t github-style
+
+echo "Updating gh-pages branch"
+cd public && git add --all && git commit -S -m "Publishing to gh-pages ($(git log --all --oneline -n 1 --format=%h))
+Master orinal version: $(git log --all --oneline -n 1 --format=%H%n%s)"
+
+#echo "Pushing to github"
+#git push --all
